85d9715c311e309dd207a1937a43039a6bb270d2
saxad <saad_zizi@hotmail.com>
2017-04-10 23:58:48 +0200

    algo du maximum

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -5,27 +5,23 @@ import org.apache.commons.lang.WordUtils;
 
 public class Hello {
     public static void main(String[] args) {
-        String  message = "hello ivy !";
-        System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
-
-				int monmax = 0;
-				CSVREADER reader = new CSVReader(new FileReader("data.csv"));
-				List<String[]> myEntries = reader.readAll();
-				//parcourt
-				for(int i = 0; i <myEntries.len(); i++)
-				{
-					System.out.pritln("nombre : " + myEntries.get(i));
-					
-					if(myEntries.get(i) > monmax)
-					{
-						monmax = myEntries.get(i);
+					int monmax = 0;
+					CSVREADER reader = new CSVReader(new FileReader("../../data.csv"));
+					List<String[]> myEntries = reader.readAll();
+					for (int = 1 = 0; i < myEntries.size(); i++){
+						String[] strings = myEntries.get(i);
+						for (int j = 0; j < strings.length; j++){
+							if( strings[j] > monmax ){
+								monmax = strings[j];
+							}
+							System.out.println(strings[j]);
+						}
+						System.out.println("le maximum est : " + monmax)
 					}
-
-				}
-
-				System.out.println("Le max est  :" +monmax); 
+//        String  message = "hello ivy !";
+//        System.out.println("standard message : " + message);
+//        System.out.println("capitalized by " + WordUtils.class.getName() 
+//                                + " : " + WordUtils.capitalizeFully(message));
     }
 }
         
