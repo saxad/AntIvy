5e6b0fa2ccdb68517074cd8576b72b6f83500dda
saxad <saad_zizi@hotmail.com>
2017-04-10 04:21:33 +0200

    opencsv exercice 13->16

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -9,6 +9,23 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+
+				int monmax = 0;
+				CSVREADER reader = new CSVReader(new FileReader("data.csv"));
+				List<String[]> myEntries = reader.readAll();
+				//parcourt
+				for(int i = 0; i <myEntries.len(); i++)
+				{
+					System.out.pritln("nombre : " + myEntries.get(i));
+					
+					if(myEntries.get(i) > monmax)
+					{
+						monmax = myEntries.get(i);
+					}
+
+				}
+
+				System.out.println("Le max est  :" +monmax); 
     }
 }
         
